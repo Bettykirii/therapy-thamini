e370e2a5efc602118d8dc4b4deeef8b2cf21e650
Bettykirii <warwarekirii@gmail.com>
2020-09-16 16:56:04 +0300

    list all counselors

--- src/main/java/models/Appointments.java
+++ src/main/java/models/Appointments.java
@@ -14,71 +14,14 @@ public class Appointments {
 //    @ManyToOne
 //    private Clients clients;
 
+    @Embedded
+     private Clients clients;
 
-
-
-
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private int id;
-
-    public int getId() {
-        return id;
+    public Clients getClients() {
+        return clients;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    @Column(name = "first_name")
-    private String firstName;
-
-    @Column(name = "last_name")
-    private String lastName;
-
-    @Column(name = "user_name")
-    private String username;
-
-    @Column(name = "password")
-    private String password;
-
-    @Column(name = "email")
-    private String Email;
-
-
-
-
-    public String getFirstName() {
-        return firstName;
-    }
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-    public String getLastName() {
-        return lastName;
-    }
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-    public String getUsername() {
-        return username;
-    }
-    public void setUsername(String username) {
-        this.username = username;
-    }
-    public String getPassword() {
-        return password;
-    }
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return Email;
-    }
-
-    public void setEmail(String email) {
-        Email = email;
+    public void setClients(Clients clients) {
+        this.clients = clients;
     }
 }
